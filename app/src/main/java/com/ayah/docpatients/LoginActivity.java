a6f7b9c8bd302c1dac2e358321d85c180b31d5b7
Ayah <aslanayah3@gmail.com>
2019-03-28 13:39:36 +0200

    login without checkbox

--- app/src/main/java/com/ayah/docpatients/LoginActivity.java
+++ app/src/main/java/com/ayah/docpatients/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.ayah.docpatients.Activities.MedicineActivity;
 import com.ayah.docpatients.data.MyDoctor;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -27,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText password;
     private Button signup;
     private Button signin;
-    private CheckBox ifDoctor, ifPatient;
+//    private CheckBox ifDoctor, ifPatient;
 
     private DatabaseReference databaseReference;
     FirebaseAuth auth;
@@ -38,8 +39,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ifDoctor = (CheckBox)findViewById(R.id.ifDoctor);
-        ifPatient = (CheckBox)findViewById(R.id.ifPatient);
+//        ifDoctor = (CheckBox)findViewById(R.id.ifDoctor);
+//        ifPatient = (CheckBox)findViewById(R.id.ifPatient);
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
@@ -99,34 +100,47 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void signIn(final String email, String password) {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
 
-                    Toast.makeText(LoginActivity.this, "signIn Successful.", Toast.LENGTH_SHORT).show();
-
                     //todo check if doctor logs in or patient
 
 //                    if ()
 
-
-                    if (ifDoctor.isChecked() && !ifPatient.isChecked()) {
+                    if(email.equals("roza@gmail.com") || email.equals("aya.a@gmail.com")) {
+                        Toast.makeText(LoginActivity.this, "signIn Successful.", Toast.LENGTH_SHORT).show();
                         Intent intent1 = new Intent(LoginActivity.this, PatientsListActivity.class);
                         startActivity(intent1);
                         finish();
-
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "signIn Successful.", Toast.LENGTH_SHORT).show();
+                        Intent intent1 = new Intent(LoginActivity.this, MedicinesListActivity.class);
                     }
 
-                    if (!ifDoctor.isChecked() && ifPatient.isChecked()) {
-                        Intent intent2 = new Intent(LoginActivity.this, MedicinesListActivity.class);
-                        startActivity(intent2);
-                        finish();
-                    }
 
-                    if ((ifPatient.isChecked() && ifDoctor.isChecked()) || (!ifPatient.isChecked() && !ifDoctor.isChecked())) {
-                        Toast.makeText(getBaseContext(), "You have to pick  on of the checkbox in order to sign up", Toast.LENGTH_LONG).show();
-                    }
+
+//                    if (ifDoctor.isChecked() && !ifPatient.isChecked()) {
+//                        Intent intent1 = new Intent(LoginActivity.this, PatientsListActivity.class);
+//                        startActivity(intent1);
+//                        finish();
+//
+//                    }
+//
+//                    if (!ifDoctor.isChecked() && ifPatient.isChecked()) {
+//                        Intent intent2 = new Intent(LoginActivity.this, MedicinesListActivity.class);
+//                        startActivity(intent2);
+//                        finish();
+//                    }
+//
+//                    if ((ifPatient.isChecked() && ifDoctor.isChecked()) || (!ifPatient.isChecked() && !ifDoctor.isChecked())) {
+//                        Toast.makeText(getBaseContext(), "You have to pick  on of the checkbox in order to sign up", Toast.LENGTH_LONG).show();
+//                    }
 
 
                 }
